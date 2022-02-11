675a64caf8f4c1e4a0fffab6fa0eb2261100603a
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-02-11 08:09:29 +0800

    Update 2b.py

--- 2b.py
+++ 2b.py
@@ -4,7 +4,7 @@ import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=plt.imread("/Users/liuchang/Desktop/ITEC_1610/a2/2a_me_gray.png")
+img=plt.imread("/Users/liuchang/Desktop/ITEC_1610/a2/2a_me_gray.png") #image import is required
 plt.hist(img.ravel(), bins=256, range=(0.0, 1.0), fc=(1., .8, 1.), ec=(1., 0.5, 1.)) 
 plt.title("Histogram")
 plt.xlabel("Grayscale Value")
