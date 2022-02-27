189dc0d1138c4a23afa2d656202ca38d08453eee
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-02-27 17:35:10 -0500

    Create calories.py

---
+++ calories.py
@@ -0,0 +1,6 @@
+Age = int(input())
+Weight = int(input())
+Heart_Rate = int(input())
+Time = int(input())
+calories = ((Age * 0.2757) + (Weight * 0.03295) + (Heart_Rate * 1.0781) - 75.4991) * Time / 8.368 
+print(f'Calories: {calories:.2f} calories')
