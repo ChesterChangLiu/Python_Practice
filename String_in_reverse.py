fb07ed4c0be32171f231dbf96e5a18bd45969103
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-02-28 10:13:38 -0500

    Create String_in_reverse.py

---
+++ String_in_reverse.py
@@ -0,0 +1,8 @@
+while True:
+    a = input()
+    if (a == 'Done' or a == 'done' or a == 'd'):
+        break
+    else:
+        for a in reversed(a):
+            print(a, end = '')
+        print()
