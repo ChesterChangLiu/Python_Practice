e2f1c79ea75343af5fca2474d71c8f626a7694f4
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-03-10 10:34:38 -0500

    Create compute_square.py

---
+++ compute_square.py
@@ -0,0 +1,8 @@
+num_to_square = int(input())
+def compute_square(num_to_square):
+    return num_to_square * num_to_square
+
+
+num_squared = compute_square(num_to_square)
+
+print(num_to_square, 'squared is', num_squared)
