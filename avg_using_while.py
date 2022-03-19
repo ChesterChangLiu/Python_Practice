d69d98238cde19821a2e6f929dfaf6f20e772992
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-03-18 20:16:50 -0400

    Update avg_using_while.py

--- avg_using_while.py
+++ avg_using_while.py
@@ -6,6 +6,6 @@ while curr_value > 0 :
     num_val = num_val + 1 
     curr_value = int(input('Enter any positive integer (-1 will stop): '))
 print('Average of input integers', val_sum / num_val)
-    
+
 # num_val = N (no.of elements)
 # val_sum = Sum of all elements
