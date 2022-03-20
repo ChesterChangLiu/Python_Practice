f85f3ea4ffc5361ddae9c28ebadd1f93dbada4b3
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-03-20 01:35:38 -0400

    making progress to ordinal

--- useful_tools/random_red_packet.py
+++ useful_tools/random_red_packet.py
@@ -11,7 +11,32 @@ def redpacket(num,total):
     myformat="The {0} person gets {1} dollars".format
     i=[i for i in range(1,num+1)]
     print(list(map(myformat,i,each)))
+'''
+def ordinal(value):
 
+    try:
+        value = int(value)
+    except ValueError:
+        return value
+
+    if value % 100//10 != 1:
+        if value % 10 == 1:
+            ordval = u"%d%s" % (value, "st")
+        elif value % 10 == 2:
+            ordval = u"%d%s" % (value, "nd")
+        elif value % 10 == 3:
+            ordval = u"%d%s" % (value, "rd")
+        else:
+            ordval = u"%d%s" % (value, "th")
+    else:
+        ordval = u"%d%s" % (value, "th")
+
+    return ordval
+    '''
+
+if __name__ == '__main__':
+    num = int(input("Enter a number: "))
+    print("Ordinal number:", ordinal(num))
 if __name__=="__main__":
     total=int(input("Enter total amount for your red packet: "))
     num=int(input("Enter the number of participants: "))
