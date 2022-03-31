8388ded2b6e018d7b9858c84be2fb16ae0d7b976
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-03-30 23:19:53 -0400

    Update Image_gray.py

--- Image_gray.py
+++ Image_gray.py
@@ -1,7 +1,7 @@
 import PIL
 from PIL import Image
 
-img = Image.open('/Users/liuchang/Desktop/ITEC_1610/a2/2a_me.png') #image import is required
+img = Image.open('/Users/liuchang/Desktop/ITEC_1610/a2/2a_me.png') #import the image here
 img.show()
 a = img.convert('L')
 a.show()
