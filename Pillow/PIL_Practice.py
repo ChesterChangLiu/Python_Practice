3340575ae7ab777238bfe5b9bd6b58a59df5a144
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-04-07 20:59:08 -0400

    Add files via upload

--- Pillow/PIL_Practice.py
+++ Pillow/PIL_Practice.py
@@ -3,21 +3,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 image = Image.open("image.jpg") # open colour image
-#image.show() # show image
+# image.show() # show image
 
 print(image.size)
 print(image.format)
 print(image.mode) 
 
 # crop image
-
 left = 50
 top = 50
 right = 500
 bottom = 500
 crop_image = image.crop((left, top, right, bottom))
-plt.imshow(crop_image)
-crop_image.show()
+plt.imshow(crop_image) # display data as an image
 
+# rotate image
+angle = 30
+rotated_image = image.rotate(angle)
+plt.imshow(rotated_image)
 
+# Text Watermark
+from PIL import ImageDraw, ImageFont
+watermark_image = image.copy() # copy image
+draw = ImageDraw.Draw(watermark_image) # create a drawing object
+font = ImageFont.truetype("arial.ttf", size=100) # choose font
+draw.text((0, 0), "Watermark", (0,0,0), font=font) # add text
+plt.imshow(watermark_image)
+#watermark_image.show()
+
+# Image Watermark
+size = (500, 500)
+crop_image = image.copy()
+crop_image.thumbnail(size) # preserve aspect ratio
+
+copied_image = image.copy()
+copied_image.paste(crop_image, (0, 0)) # left top corner
+plt.imshow(copied_image)
+copied_image.show()
+
+#Convert Image to Black and White
+bw_image = image.convert("L") # convert to black and white
+plt.imshow(bw_image, cmap="gray") #use cmap = "gray" for matplotlib to correctly show black and white
+#bw_image.show()
+
+#Convert to Different Formats
+new_foramt_image = image.convert("HSV") #Hue Saturated Value
+print(new_foramt_image.mode)
+
+#Convert to Numpy Format
+numpy_array = np.array(image)
+print(numpy_array.shape)
+
+numpy_image = Image.fromarray(numpy_array) # convert numpy array to image
+plt.imshow(numpy_image)
 
