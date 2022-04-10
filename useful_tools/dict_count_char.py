9f21c9820d5e31cfd5a5fa65a3117f9915a38580
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-04-10 17:01:44 -0400

    add file

---
+++ useful_tools/dict_count_char.py
@@ -0,0 +1,12 @@
+#P16: Using a dictionary to count the characters.
+
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+print(histogram('parrot'))
