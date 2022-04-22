152eb0d07394aa68707c1283b0ffe407fa30e50a
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-04-21 23:58:23 -0400

    Update for_loop_savings.py

--- for_loop_savings.py
+++ for_loop_savings.py
@@ -1,8 +1,14 @@
-initial_savings = 10000
-interest_rate = float(input('Enter rate of interest: '))
-years = int(input('Enter years: '))
+initial_savings = 10000
 
-savings = initial_savings
-for i in range(1, years+1):
-    print(' Savings in year {}: ${:.2f}'.format(i, savings))
-    savings = savings + (savings*interest_rate)
+interest_rate = float(input('Enter rate of interest: '))
+
+years = int(input('Enter years: '))
+
+savings = initial_savings
+
+for i in range(1, years+1):
+
+    print(' Savings in year {}: ${:.2f}'.format(i, savings))
+
+    savings = savings + (savings*interest_rate)
+
