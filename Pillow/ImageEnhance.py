e3c2966d6f2c01cbd74de15915b4bdb09fa6657b
Chang Liu <67884903+ChesterChangLiu@users.noreply.github.com>
2022-06-09 10:36:19 -0400

    Update ImageEnhance.py

--- Pillow/ImageEnhance.py
+++ Pillow/ImageEnhance.py
@@ -9,6 +9,7 @@ image1 = ImageEnhance.Color(image_color_enhance).enhance(0.5)
 image2 = ImageEnhance.Contrast(image_color_enhance).enhance(0.5)
 image3 = ImageEnhance.Brightness(image_color_enhance).enhance(0.5)
 image4 = ImageEnhance.Sharpness(image_color_enhance).enhance(0.5)
+
 plt.subplot(2,2,1)
 plt.imshow(image1)
 plt.title("Color")
